3bccb8ebdbe5256d9d1d89608b1b067ff523ef54
Hasan <hozdemir8233@gmail.com>
2022-02-01 00:00:38 -0800

    new initial

---
+++ src/test/java/swaptwonumber.java
@@ -0,0 +1,15 @@
+public class swaptwonumber {
+
+    public static void main(String[] args) {
+
+                int a=3;
+                int b=7;
+                int c=0;
+                System.out.println(a+" " + "before swapping"+" "+b);//3 before swapping 7
+
+        int d=a+b;
+        int k=d-b;
+
+
+    }
+}
