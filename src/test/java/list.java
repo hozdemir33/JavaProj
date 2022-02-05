590e10b7d35c4bee6118f4689be89b80e73d3a4c
Hasan <hozdemir8233@gmail.com>
2022-02-04 23:29:39 -0800

    new initial commit

--- src/test/java/list.java
+++ src/test/java/list.java
@@ -8,7 +8,8 @@ public class list {
 
         //Creating a List- String elements
         List<String> list =new ArrayList<String>();
-
+        //Creating a List- Interger elements
+       // List<Integer> listt= new ArrayList<Integer>();
         list.add("Hasan");
         list.add("Husey");
         list.add("Burak");
@@ -16,17 +17,34 @@ public class list {
         list.add("Mahmut");
 
       for(String name:list){
-          System.out.println(name);
-
+     //     System.out.println(name);
       }
 
+      List<Integer> numbers=new ArrayList<Integer>();
+
+      numbers.add(1);
+      numbers.add(2);
+      numbers.add(3);
+      numbers.add(4);
+      numbers.add(5);
+
+         for(Integer numerical:numbers){
+             System.out.println(numerical);
+         }
 
 
+        //Creating a List
+        List<String> list1=new ArrayList<String>();
+
+         list1.add("apple");
+         list1.add("oranges");
+         list1.add("banana");
+
+         for(String fruits:list1){
+             System.out.println(fruits);
 
 
-
-          //Creating a List- Interger elements
-        List<Integer> listt= new ArrayList<Integer>();
+         }
 
     }
 }
