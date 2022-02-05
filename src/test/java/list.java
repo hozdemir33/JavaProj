ef30fcb79b21aa0bf02e8459d66c5554181407a7
Hasan <hozdemir8233@gmail.com>
2022-02-04 23:10:22 -0800

    newlist

---
+++ src/test/java/list.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class list {
+
+
+    public static void main(String[] args) {
+
+        //Creating a List- String elements
+        List<String> list =new ArrayList<String>();
+
+        list.add("Hasan");
+        list.add("Husey");
+        list.add("Burak");
+        list.add("Vedat");
+        list.add("Mahmut");
+
+      for(String name:list){
+          System.out.println(name);
+
+      }
+
+
+
+
+
+
+          //Creating a List- Interger elements
+        List<Integer> listt= new ArrayList<Integer>();
+
+    }
+}
