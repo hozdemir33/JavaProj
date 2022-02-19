dc4a50365bdf6b3c0a8295cd8eb42878a7bec4c9
Hasan <hozdemir8233@gmail.com>
2022-02-18 21:38:34 -0800

    new commit

---
+++ src/test/java/Arrays.java
@@ -0,0 +1,24 @@
+public class Arrays {
+
+    public static void main(String[] args) {
+
+
+        String[] name = {"ali", "veli", "nano", "hamdi", "sultan"};
+
+        {
+            String counter = "";
+
+            for (int i = 0; i < name.length; i++) {
+                char jj = counter.charAt(i);
+                System.out.println(jj);
+
+            } final(Exception e){
+
+                System.out.println("Somethinhgnot write");
+
+
+            }
+        }
+    }
+
+
