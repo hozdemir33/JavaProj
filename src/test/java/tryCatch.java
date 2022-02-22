0ade3e0104a26171c796516aa2cd39b2b70c753c
Hasan <hozdemir8233@gmail.com>
2022-02-21 19:43:55 -0800

    new commit

---
+++ src/test/java/tryCatch.java
@@ -0,0 +1,20 @@
+public class tryCatch {
+
+    public static void main(String[] args) {
+
+
+        try {
+
+            int a[] = {2, 3, 4, 5};
+            System.out.println(a[5]);// this is where we get the exception
+
+        } catch (Exception e) {
+
+            // System.out.println("There is an error and pass it on now!");// this is the message you can pass it to non technical people.
+
+        }
+
+    }
+
+}
+
